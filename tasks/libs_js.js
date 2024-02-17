d6a3d996de1c7badecc7236d41c1eadfa7bbde18
Virab-Maestro <khachatryanvirab7@gmail.com>
2024-02-17 19:48:28 +0300

    add: libs_js

---
+++ tasks/libs_js.js
@@ -0,0 +1,16 @@
+const {
+	src,
+	dest
+} = require('gulp');
+const uglify = require('gulp-uglify-es').default;
+const concat = require('gulp-concat');
+const map = require('gulp-sourcemaps');
+
+module.exports = function libs_js(done) {
+    return src(plugins)
+        .pipe(map.init())
+        .pipe(uglify())
+        .pipe(concat('libs.min.js'))
+        .pipe(map.write('../sourcemaps'))
+        .pipe(dest('build/js/'))
+}
