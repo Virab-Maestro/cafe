1c3475297edd4ff70a2d2ea6c18a50830e2d1ced
Virab-Maestro <khachatryanvirab7@gmail.com>
2024-02-17 20:28:30 +0300

    add: watch task

---
+++ tasks/watch.js
@@ -0,0 +1,12 @@
+const {
+	watch,
+	parallel,
+	series
+} = require('gulp');
+
+module.exports = function watching() {
+	watch('src/**/*.html', parallel('html'));
+	watch('src/**/*.scss', parallel('style'));
+	watch('src/**/*.js', parallel('dev_js'));
+	watch('src/**/*.json', parallel('html'));
+}
